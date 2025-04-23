072b21dd552e25a00bb0c300dbdfe5ccb25923bf
PikoArpento <simo.torri104@gmail.com>
2025-04-23 17:16:51 +0200

    Delete meteo directory

--- meteo/sole.js
+++
@@ -1,8 +0,0 @@
-// 🔴 Rimuove gli elementi meteo precedenti
-document.querySelectorAll(".sun").forEach(el => el.remove());
-
-// 🔵 Crea il nuovo effetto sole
-const sun = document.createElement("div");
-sun.classList.add("sun", "weather-element");
-
-document.body.appendChild(sun);
